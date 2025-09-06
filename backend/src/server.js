4bca7dc644291d182c199b78f4f9020d85e5c440
HoaAng911 <mayanh279@gmail.com>
2025-09-06 22:05:10 +0700

    update model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import tasksRoute from './routes/tasksRoutes.js';
 const app = express();
-const PORT = 5001;
-
+import dotenv from "dotenv";
+import connectDB from './config/db.js';
 // Middleware để đọc JSON
 app.use(express.json());
-
+dotenv.config();
+connectDB()
 // Sử dụng route
 app.use('/api/tasks', tasksRoute);
-
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server chạy trên http://localhost:${PORT}`);
 });
